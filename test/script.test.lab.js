1787ab0569aa51c4a4b1d57815c3e03dcab98ca9
jshighbasic <100546975+jshighbasic@users.noreply.github.com>
2022-02-28 11:24:31 +0800

    Add files via upload

---
+++ test/script.test.lab.js
@@ -0,0 +1,14 @@
+/*!
+ * jshighbasic library test lab v0.0.1
+ * https://jshighbasic.github.io/library/testlab.html
+ *
+ * Released under the MIT license
+ * https://opensource.org/licenses/MIT
+ */
+var outputArray=[];console.log=function(e){outputArray.push(e)};try{var url=location.href;var backTo=url.split('?dt=')[1].split('&script=')[0];var script=url.split('&script=')[1].split('&testval=')[0];var testval=url.split('&testval=')[1].split(';,');script=decodeURIComponent(script);try{if(testval==undefined){eval(script)}else{for(let i=0;i<testval.length;i++){eval(decodeURIComponent(testval[i]));eval(script)}}var token=Math.random().toString(36).substring(2,15)+Math.random().toString(36).substring(2,15);location.href=`${backTo}?success=t&token=${token}&script=${url.split('&script=')[1].split('&testval=')[0]}`;localStorage.setItem(token,JSON.stringify(outputArray))}catch(e){console.table({Error:e.message});location.href=`${backTo}?error=${e}&script=${url.split('&script=')[1].split('&testval=')[0]}`}}catch(e){console.error(e)}
+// Success : ?success=t&token=xxxxx&script=xxxxxx;
+// Error : ?error=xxxxx&script=xxxxxx;
+/*!
+ * Get the test result from localStorage with the token.
+ * localStorage.getItem(**token**);
+ */
